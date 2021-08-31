391e102a27dbc246e5aec23051f5cca9c90fb71e
RAMI <ayoubramimksb@gmail.com>
2021-08-31 17:38:00 +0100

    fix env variable name

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import jwtDecode from 'jwt-decode';
 function App({...props}) {
   
   const link = createHttpLink({
-    uri: process.env.BROKER_API || 'http://localhost:3030/graphql',
+    uri: process.env.REACT_APP_API_URL || 'http://localhost:3030/graphql',
     credentials: 'include',
     // fetchOptions: {
     //   credentials: 'include'
